46604d1d93945f33047f0e8d517135ecaa8257f7
João Pedro <78887413+joaopedrommoreira@users.noreply.github.com>
2025-06-07 10:47:34 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         <a href="[URL-do-seu-LinkedIn]" target="_blank" rel="noopener noreferrer">LinkedIn</a> | 
         <a href="[URL-do-seu-GitHub]" target="_blank" rel="noopener noreferrer">GitHub</a>
       </p>
-      <p>&copy; {new Date().getFullYear()} [Seu Nome]. Todos os direitos reservados.</p>
+      <p>&copy; {new Date().getFullYear()} Automize Soluções & Tecnologias. Todos os direitos reservados.</p>
     </footer>
   );
 }
